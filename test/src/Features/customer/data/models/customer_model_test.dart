8e5b0018b09a3170b24c496da8915626fc89250a
Raymond Mwaikasu <mwaikasu21@gmail.com>
2024-03-13 09:57:19 +0300

    valid fromjson

---
+++ test/src/Features/customer/data/models/customer_model_test.dart
@@ -0,0 +1,46 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd/src/Features/customer/data/models/customer_model.dart';
+import 'package:tdd/src/Features/customer/domain/entities/customer_entity.dart';
+
+void main() {
+  var testCustomerModel = CustomerModel(
+      id: 1,
+      username: 'JohnDoe',
+      email: 'mwaikasu21@gmail.com',
+      firstName: 'Raymond',
+      lastName: 'Mwaikasu',
+      address: 'Moshi',
+      phoneNumber: "0678909090",
+      createdAt: '32324-342',
+      updatedAt: '4534-590385903');
+
+  test('should be a subclass of customer Entity', () async {
+    // assert
+    expect(testCustomerModel, isA<CustomerEntity>());
+  });
+
+  test('should return a valid Customermodel from json', () {
+    expect(
+        CustomerModel.fromJson(<String, dynamic>{
+          'id': 1,
+          'username': 'JohnDoe',
+          'email': 'mwaikasu21@gmail.com',
+          'first_name': 'Raymond',
+          'last_name': 'Mwaikasu',
+          'address': 'Moshi',
+          'phone_number': "0678909090",
+          'created_at': '32324-342',
+          'updated_at': '4534-590385903'
+        }),
+        isA<CustomerModel>()
+            .having((s) => s.id, 'id', 1)
+            .having((p0) => p0.username, 'username', 'JohnDoe')
+            .having((p0) => p0.email, 'email', 'mwaikasu21@gmail.com')
+            .having((p0) => p0.firstName, 'firstName', 'Raymond')
+            .having((p0) => p0.lastName, 'lastName', 'Mwaikasu')
+            .having((p0) => p0.address, 'address', 'Moshi')
+            .having((p0) => p0.phoneNumber, 'phoneNumber', '0678909090')
+            .having((p0) => p0.createdAt, 'createdAt', '32324-342')
+            .having((p0) => p0.updatedAt, 'updatedAt', '4534-590385903'));
+  });
+}
